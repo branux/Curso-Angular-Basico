9aafb84e5714450099f759d5448ccafe274ad6a5
Fabiano Alberto Nalin <fnalin@msn.com>
2016-02-29 09:50:44 -0300

    06 - Route

---
+++ CursoNGBasico/06 - Route/app/controllers/main/maincontroller.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    'use strict';
+
+    angular.module("app").controller("MainController", ["$scope", MainController]);
+
+    function MainController($scope) {
+        $scope.index = "variável do index";
+        $scope.about = "variável do about";
+        $scope.contact = "variável do contact";
+        $scope.error = "variável do error";
+    };
+
+}());
---
+++ CursoNGBasico/06 - Route/app/route.js
@@ -0,0 +1,31 @@
+﻿(function () {
+    "use strict";
+
+    angular.module('app').config(function ($routeProvider) {
+
+        $routeProvider
+
+            .when('/', {
+                controller: 'MainController',
+                templateUrl: 'pages/main/index.html'
+            })
+
+            .when('/about', {
+                 controller: 'MainController',
+                 templateUrl: 'pages/main/about.html'
+            })
+
+            .when('/contact', {
+                controller: 'MainController',
+                templateUrl: 'pages/main/contact.html'
+            })
+
+            .when('/error', {
+                controller: 'MainController',
+                templateUrl: 'pages/main/error-404.html'
+            })
+
+            .otherwise({redirectTo:'/error'});
+    });
+
+}());
